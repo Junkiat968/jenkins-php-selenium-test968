fa75c002d6d4f69bd01db2d7cced8cc1e1a8ffd4
rainyraina <60285387+rainyraina@users.noreply.github.com>
2022-11-13 22:32:09 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v https://github.com/rainyraina/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+pwd
+git clone https://github.com/rainyraina/jenkins-php-selenium-test.git
+#ls
+docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test@2/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+#docker exec my-apache-php-app /bin/bash -c 'chmod 777 /var/www/html/index.php'
+docker exec my-apache-php-app /bin/bash -c 'ls'
+docker exec my-apache-php-app /bin/bash -c 'curl http://172.17.0.2'
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost to see your PHP application in action.'
-
+echo 'Visit http://172.17.0.2 to see your PHP application in action.'
