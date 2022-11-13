cb92dc821b20bb945dbcf22dea27f5781fcc7c94
rainyraina <rainyraina23@gmail.com>
2022-11-14 00:13:47 +0800

    g

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -4,7 +4,7 @@ set -x
 
 #git clone https://github.com/rainyraina/jenkins-php-selenium-test.git
 
-docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test@2/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /home/raina/Desktop/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 
 sleep 1
 set +x
