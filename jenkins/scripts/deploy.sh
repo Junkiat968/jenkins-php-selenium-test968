f58e6e4a792c641b7e893d5538362eea6ce52d49
rainyraina <60285387+rainyraina@users.noreply.github.com>
2022-11-14 01:06:46 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -5,7 +5,8 @@ set -x
 git clone https://github.com/rainyraina/jenkins-php-selenium-test.git
 
 docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test@2/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
-
+docker exec my-apache-php-app /bin/bash -c 'ls'
+docker exec my-apache-php-app /bin/bash -c 'curl http://localhost'
 
 sleep 1
 set +x
