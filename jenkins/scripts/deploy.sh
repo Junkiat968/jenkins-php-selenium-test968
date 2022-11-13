57994047b166ac1e115278e241c304146a27f6f1
rainyraina <60285387+rainyraina@users.noreply.github.com>
2022-11-13 22:05:23 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -9,10 +9,10 @@ docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/j
 docker exec my-apache-php-app /bin/bash -c 'cat /etc/hosts'
 docker exec my-apache-php-app /bin/bash -c 'cat /etc/nsswitch.conf'
 docker exec my-apache-php-app /bin/bash -c 'ls'
-docker exec my-apache-php-app /bin/bash -c 'curl http://172.19.0.2:80/index.php'
+docker exec my-apache-php-app /bin/bash -c 'curl http://172.17.0.2:80/index.php'
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://172.19.0.2 to see your PHP application in action.'
+echo 'Visit http://172.17.0.2 to see your PHP application in action.'
 
