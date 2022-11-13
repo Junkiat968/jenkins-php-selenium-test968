30f8d75e028b3979d26147855913677ae8ab512d
rainyraina <60285387+rainyraina@users.noreply.github.com>
2022-11-13 17:49:02 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,6 +2,7 @@
 
 set -x
 docker run -d -p 80:80 --name my-apache-php-app -v //mnt/c/Users/raina/Desktop/school/ICT3203/practical/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+docker exec my-apache-php-app /bin/bash -c 'chmod 777 .'
 docker exec my-apache-php-app /bin/bash -c 'curl http://localhost:80'
 sleep 1
 set +x
